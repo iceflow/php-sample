92978ab551e7a6000701cc9a170ad5ff255eddf6
leo <chenlt78@qq.com>
2015-09-20 23:44:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   echo "Instance ID: <b>" .$instance_id ."</b><br/>";
   $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
   $zone = file_get_contents($url);
-  echo "1 Zone: <b>" .$zone ."</b><br/>";
+  echo "2 Zone: <b>" .$zone ."</b><br/>";
 
   phpinfo();
 ?>
